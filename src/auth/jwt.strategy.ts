ad7e7d03baa60c81678df32333206eceb14051f7
Hazel Azka <hazelazka091@gmail.com>
2026-03-11 21:11:41 +0700

    fixed: update jwt strategy use env

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'RAHASIA_NEGARA_123',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
