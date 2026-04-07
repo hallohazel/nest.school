d1caf3e53a7a2de40b0423672fe76121a17be338
Hazel Azka <hazelazka091@gmail.com>
2026-04-07 11:44:57 +0700

    fix: add error handling to bootstrap function in main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,4 +19,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('App gagal jalan', err);
+});
